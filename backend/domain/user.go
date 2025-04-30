ab0992e0791423575791b61c0fff70da9b9cc3a7
ktsu2i <kaito.tsu2i@gmail.com>
2025-04-30 22:20:59 +0900

    created user model

---
+++ backend/domain/user.go
@@ -0,0 +1,12 @@
+package domain
+
+import "time"
+
+type User struct {
+	ID             string    `json:"id"`
+	Username       string    `json:"username"`
+	Email          string    `json:"email"`
+	HashedPassword string    `json:"hashed_password"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
